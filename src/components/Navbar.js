93abe1770d8a70227f12b3e3077bf98980c384aa
rrex971 <75212090+rrex971@users.noreply.github.com>
2024-11-25 19:34:18 +0530

    some miscellaneous changes related to responsive design.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,8 @@ const Navbar = () => {
   return (
     <nav className="bg-logan-500 bg-cover p-5 rounded-b-xl">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-banana-mania-100 text-2xl font-head">o!CBS</div>
-        <ul className="flex space-x-8 font-head text-logan-800">
-          <li><NavLink to="/" className={({ isActive }) => (isActive ? "text-banana-mania-100 opacity-100" : "navbar-link opacity-50 hover:opacity-100 transition-opacity duration-300 transition-colors duration-500 ease-in-out")}>Home</NavLink></li>
+        <div className="text-banana-mania-100 text-2xl font-head"><NavLink to="/" className="navbar-link hover:text-white-50 transition-colors duration-300 ease-in-out">o!CBS</NavLink></div>
+        <ul className="flex md:space-x-8 space-x-3 font-head text-logan-800">
           <li><NavLink to="/info" className={({ isActive }) => (isActive ? "text-banana-mania-100 opacity-100" : "navbar-link opacity-50 hover:opacity-100 transition-opacity duration-300 transition-colors duration-500 ease-in-out")}>Info</NavLink></li>
           <li><NavLink to="/mappools" className={({ isActive }) => (isActive ? "text-banana-mania-100 opacity-100" : "navbar-link opacity-50 hover:opacity-100 transition-opacity duration-300 transition-colors duration-500 ease-in-out")}>Mappools</NavLink></li>
           <li><NavLink to="/staff" className={({ isActive }) => (isActive ? "text-banana-mania-100 opacity-100" : "navbar-link opacity-50 hover:opacity-100 transition-opacity duration-300 transition-colors duration-500 ease-in-out")}>Staff</NavLink></li>
--- src/screens/Info.js
+++ src/screens/Info.js
@@ -3,11 +3,12 @@ import React from 'react';
 const Info = () => {
   return (
   <div className='bg-logan-500 bg-info-bg bg-cover h-screen'>
-    <div className="container mx-auto my-auto flex justify-between items-center font-head">
+    <div className="container mx-auto my-auto px-10 flex flex-down justify-between items-center font-head">
         <div className='aboutheader py-20'>
-            <span className='text-6xl text-logan-600'>About </span><span className="text-4xl text-logan-400">our</span>
-            <span className='text-6xl text-logan-400 block'>Tournament</span>
+            <span className='text-4xl md:text-6xl text-logan-600'>About </span><span className="text-2xl md:text-4xl text-logan-400">our</span>
+            <span className='text-4xl md:text-6xl text-logan-400 block'>Tournament</span>
         </div>
+        
     </div>
   </div>
 )};
