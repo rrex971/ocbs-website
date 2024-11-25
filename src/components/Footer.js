0d18db20c585c919d018c8c04128222f6bbf5422
rrex971 <75212090+rrex971@users.noreply.github.com>
2024-11-26 01:44:59 +0530

    more responsive page changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,16 +5,14 @@ const Footer = () => {
   return (
     <nav className="bg-logan-500 bg-cover mt-48 p-8 md:p-16 rounded-b-xl">
         <div className="container mx-auto flex justify-between items-center">
-            <div className="text-banana-mania-100 text-lg md:text-2xl font-body">
+            <div className="text-banana-mania-100 text-sm md:text-2xl font-body">
                 <span className='font-head block'>osu!Chennai Beachside Showdown</span>
-                website made with ♥ by rrex
+                website made with ♥ by <a href='https://github.com/rrex971'><u>rrex</u></a>
             </div>
             <ul className="flex md:space-x-8 space-x-3 font-head text-logan-800">
             <li>
                 <NavLink to="https://discord.gg/VPDy35PaPN" className="socialicon opacity-50 hover:opacity-100 transition-opacity duration-300">
-                    <svg width="48" height="48" class='fill-logan-800'>       
-                        <image href="discord.svg" src="xd.png" width="48" height="48"/>    
-                    </svg>
+                    <img src='discord.svg' alt='Discord' className='max-w-8 md:max-w-12'></img>
                 </NavLink>
             </li>
             </ul>
--- src/screens/Info.js
+++ src/screens/Info.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 const Info = () => {
   return (
-  <div className='bg-logan-500 bg-info-bg bg-cover h-screen'>
+  <div className='bg-logan-500 bg-info-bg bg-cover md:h-screen'>
     <div className="aboutcontainer mx-auto my-auto px-12 md:px-48 flex flex-col lg:flex-row justify-between items-center ">
       <div className='abouttext'>
         <div className='aboutheader font-head pt-20 pb-5'>
             <span className='text-4xl md:text-6xl text-logan-600'>About </span><span className="text-2xl md:text-4xl text-logan-400">our</span>
             <span className='text-4xl md:text-6xl text-logan-400 block'>Tournament</span>
         </div>
-        <div className='font-body text-logan-700 md:max-w-md pr-10 block'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis turpis pulvinar, scelerisque ipsum blandit, posuere odio. Phasellus elementum dictum aliquet. Nam aliquam convallis ultricies. Nam auctor consequat finibus. Nulla odio orci, hendrerit nec magna ornare, laoreet molestie nisl. Fusce dignissim sed lorem eget bibendum. Praesent orci massa, convallis cursus hendrerit eget, molestie non risus. Suspendisse ac lectus eu sem cursus finibus. Integer feugiat laoreet leo eget congue. Fusce semper eget ipsum eu eleifend. In hac habitasse platea dictumst. Cras id libero mi. Mauris id nulla id leo egestas tincidunt ut id orci. Nam condimentum finibus sollicitudin. 
+        <div className='font-body text-logan-800 text-sm md:text-lg md:max-w-md pr-10 block'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis turpis pulvinar, scelerisque ipsum blandit, posuere odio. Phasellus elementum dictum aliquet. Nam aliquam convallis ultricies. Nam auctor consequat finibus. Nulla odio orci, hendrerit nec magna ornare, laoreet molestie nisl. Fusce dignissim sed lorem eget bibendum. Praesent orci massa, convallis cursus hendrerit eget, molestie non risus. Suspendisse ac lectus eu sem cursus finibus.
         </div>
       </div>
       <div className='imagecontainer pt-8 lg:pt-0'>
@@ -21,7 +21,7 @@ const Info = () => {
       <div className="galleryText font-head">
         <span className='text-4xl md:text-6xl text-banana-mania-100'>Gallery</span>
       </div>
-      <div className='galleryImages flex flex-col justify-between items-center block'>
+      <div className='galleryImages mt-16 flex flex-col justify-between items-center block'>
         <img src={"checklater.svg"} alt='The event has not yet concluded. Check back later.'></img>
       </div>
     </div>
