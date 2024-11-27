1def18b08fa10b676a8d8f9604da50c0e4171e95
rrex971 <75212090+rrex971@users.noreply.github.com>
2024-11-28 02:22:57 +0530

    improve login button

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const LoginButton = () => {
     return(
-    <button className="rounded-full font-body font-bold text-logan-700 bg-banana-mania-100 opacity-75 hover:bg-banana-mania-50 transition-colors duration-300 ease-in-out px-2 py-2">
+    <button className="rounded-full font-body font-bold text-xl text-logan-700 bg-banana-mania-100 opacity-75 hover:bg-banana-mania-50 transition-colors duration-300 ease-in-out px-2 py-2">
         <div className='flex justify-between items-center space-x-2'>
-          <img src='osulogo.svg' alt="osu logo" width={45} className='inline'></img>
-          <span className='hidden md:inline pr-2'>login with osu!</span>
+          <img src='osulogo.svg' alt="osu logo" width={35} className='inline'></img>
+          <span className='hidden md:inline pr-2 relative -top-0.25'>Login</span>
         </div>
     </button>
         
