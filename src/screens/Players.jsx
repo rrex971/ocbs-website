519f1debebd6c2e0437fe24503ce4b9933b15eb7
rrex971 <75212090+rrex971@users.noreply.github.com>
2024-12-23 00:46:27 +0530

    implement Players.jsx page

--- src/screens/Players.jsx
+++ src/screens/Players.jsx
@@ -1,10 +1,51 @@
-import React from 'react';
+import { React, useState, useEffect } from "react";
+import LoadingScreen from "../screens/LoadingScreen";
 import transition from '../transition';
+import Player from '../components/Player';
+
 
 const Players = () => {
-  return <div className='flex justify-center items-center h-screen'>
-    <img src='pagelater.svg' alt='Page Later'></img>
-  </div>;
+    const [registrations, setRegistrations] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+      const fetchRegistrations = async () => {
+        const response = await fetch(`https://ocbs.rrex.cc/api/registrations`, {
+                method: 'GET',
+                headers: {
+                    'Access-Control-Allow-Origin':'*',
+                },
+            });
+            const data = await response.json();
+            console.log('Response from server:', data);
+
+            if (data.error) {
+                console.log('Error:', data.error);
+            } else {
+                setRegistrations(data);
+            }
+            setLoading(false);
+        }
+        
+      
+    fetchRegistrations(); 
+    }, []);
+
+    if(loading){
+        return <LoadingScreen />;
+    }
+    return (
+        <div className='bg-register-bg bg-left-top bg-cover md:h-screen'>
+            <div className="flex justify-center items-center pt-24 space-x-8 font-head text-4xl text-logan-700">
+                Registered Players
+            </div>
+            <div className='flex flex-wrap max-w-1/2 justify-center items-center content-center align-center space-x-4 my-4'>        
+              {registrations.map((registration) => (
+                <Player key={registration.userId} id={registration.userId} username={registration.username} avatarUrl={registration.avatarUrl} rank={registration.rank} discord={registration.discordUsername} />
+              ))}
+            </div>
+        </div>
+    );
 };
 
 export default transition(Players); 
--- src/transition.jsx
+++ src/transition.jsx
@@ -6,10 +6,10 @@ const transition = (OrigComponent) => {
         <>
             <motion.div
                 className="w-full h-screen origin-bottom"
-                initial={{ y: 10, opacity: 0}}
-                animate={{ y: 0, opacity: 1 }}
-                exit={{ y: 10, opacity: 0 }}
-                transition={{duration: 0.3, ease: "easeInOut"}}>
+                initial={{ x: -10, opacity: 0}}
+                animate={{ x: 0, opacity: 1 }}
+                exit={{ x: 10, opacity: 0 }}
+                transition={{type: "spring", duration: 0.3, ease: "easeInOut"}}>
                 <OrigComponent {...props}/>
             </motion.div>
         </>
