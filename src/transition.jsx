6cb78b560da9de7b26aa832860ac19a46b623250
rrex971 <75212090+rrex971@users.noreply.github.com>
2025-01-04 18:46:49 +0530

    Fixed longstanding footer issues.

--- src/transition.jsx
+++ src/transition.jsx
@@ -5,7 +5,7 @@ const transition = (OrigComponent) => {
     return (props) => (
         <>
             <motion.div
-                className="w-full h-lvh origin-left"
+                className=""
                 initial={{ x: -10, opacity: 0}}
                 animate={{ x: 0, opacity: 1 }}
                 exit={{ x: 10, opacity: 0 }}
